71504428a70a109a88ba37e9f8243948993650ca
NEERAJ <70850450+NeerajRegaltos@users.noreply.github.com>
2022-08-12 16:25:29 +0530

    Updated

--- index.js
+++ index.js
@@ -14,24 +14,21 @@ let list = document.getElementById("list");
 let num = document.getElementById("num");
 
 
-function optimizedFunc() {
-    list.innerHTML = "";
-    num.textContent = arr.length;
-    for (let i = 0; i < arr.length; i++) {
-        list.innerHTML += arr[i];
-        document.getElementsByClassName("getId")[i].setAttribute("id", i);
-    }
-}
-
 addBtn.addEventListener("click", function (e) {
     let text = todoText.value.trim();
 
     if (text.length !== 0) {
         document.getElementById("para").innerText = "";
-        arr.push(`<li class="li"><span class="trash getId" onclick="trashCan(this.id)"><i class="fa-solid fa-trash"></i></span> ${todoText.value} </li>`);
+        arr.push(`<li class="li"><span class="trash getId" onclick="trashCan(this.id)"><i class="fa-solid fa-trash"></i></span> ${todoText.value} <span class="edit giveMeId" onclick="editMe(this.id)"><i class="fa-solid fa-pencil"></i> </span></li>`);
         todoText.value = "";
 
-        optimizedFunc();
+        list.innerHTML = "";
+        num.textContent = arr.length;
+        for (let i = 0; i < arr.length; i++) {
+            list.innerHTML += arr[i];
+            document.getElementsByClassName("getId")[i].setAttribute("id", i);
+            document.getElementsByClassName("giveMeId")[i].setAttribute("id", i);
+        }
 
     } else {
         document.getElementById("para").innerText = "Please Enter some text";
@@ -39,21 +36,50 @@ addBtn.addEventListener("click", function (e) {
     }
 
     e.preventDefault();
-
 });
 
 
 function trashCan(clickedId) {
-    console.log(clickedId)
+    console.log("trash", clickedId)
     arr.splice(clickedId, 1);
 
-    optimizedFunc();
+    list.innerHTML = "";
+    num.textContent = arr.length;
+    for (let i = 0; i < arr.length; i++) {
+        list.innerHTML += arr[i];
+        document.getElementsByClassName("getId")[i].setAttribute("id", i);
+        document.getElementsByClassName("giveMeId")[i].setAttribute("id", i);
+
+    }
 }
 
 
 
+function editMe(clickedId) {
+    let t = document.getElementById(clickedId).parentElement.innerText;
 
+    document.getElementById(clickedId).parentElement.innerHTML = `<input type='text' value='${t}' id=${clickedId} autocomplete="off" >`;
 
+    document.getElementById(clickedId).addEventListener("change", function () {
 
+        if (document.getElementById(clickedId).value.trim().length !== 0) {
+            arr[clickedId] = `<li class="li"><span class="trash getId" onclick="trashCan(this.id)"><i class="fa-solid fa-trash"></i></span> ${this.value} <span class="edit giveMeId" onclick="editMe(this.id)"><i class="fa-solid fa-pencil"></i> </span></li>`;
+
+            document.getElementById("para").innerText = "";
+            list.innerHTML = "";
+            num.textContent = arr.length;
+            for (let i = 0; i < arr.length; i++) {
+                list.innerHTML += arr[i];
+                document.getElementsByClassName("getId")[i].setAttribute("id", i);
+                document.getElementsByClassName("giveMeId")[i].setAttribute("id", i);
+
+            }
+
+        } else {
+            document.getElementById("para").innerText = "Please Enter some text";
+        }
+
+    })
+}
 
 
