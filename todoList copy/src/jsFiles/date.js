e13919269d28ea84989a1358c1d4a9c8e62f6f1c
NEERAJ <70850450+NeerajRegaltos@users.noreply.github.com>
2022-08-26 19:37:40 +0530

    Delete todoList copy directory

--- todoList copy/src/jsFiles/date.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    let date = new Date();
-    let weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    document.getElementById("day").textContent = weekDays[date.getDay()];
-
-
-    document.getElementById("fullDate").textContent = `${date.getUTCMonth() + 1}-${date.getDate()}-${date.getFullYear()}`;
-
-})();
